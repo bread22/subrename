6f3baaa6beda0d97848393d27c2dd05b1bce3f47
bread22 <wuqingyi22@gmail.com>
2020-02-28 23:41:22 -0600

    add some unittest

---
+++ test_subrename.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+import subrename
+
+
+class subrenameTest(TestCase):
+    @staticmethod
+    def test_find_episode_metadata_for_media():
+        data_cache = {
+            'show_A': [
+                {'airedSeason': 1, 'airedEpisodeNumber': 1, 'episodeName': 'EP1'},
+                {'airedSeason': 1, 'airedEpisodeNumber': 2, 'episodeName': 'EP2'},
+                {'airedSeason': 1, 'airedEpisodeNumber': 3, 'episodeName': 'EP3'},
+                {'airedSeason': 1, 'airedEpisodeNumber': 4, 'episodeName': 'EP4'},
+                {'airedSeason': 1, 'airedEpisodeNumber': 1, 'episodeName': 'ABC'},
+            ]
+        }
+        metadata = {'series': 'show_A', 'season': 1, 'episode': 1}
+        subrename.find_episode_metadata_for_media(metadata, data_cache) == {'series': 'show_A',
+                                                                            'season': 1,
+                                                                            'episode': 1,
+                                                                            'names': {'EP1', 'ABC'}}
+        metadata = {'series': 'show_A', 'season': 1, 'episode': 2}
+        subrename.find_episode_metadata_for_media(metadata, data_cache) == {'series': 'show_A',
+                                                                            'season': 1,
+                                                                            'episode': 1,
+                                                                            'names': {'EP2'}}
+
+        metadata = {'series': 'show_A', 'season': 2, 'episode': 1}
+        subrename.find_episode_metadata_for_media(metadata, data_cache) == {'series': 'show_A',
+                                                                            'season': 1,
+                                                                            'episode': 1,
+                                                                            'names': set()}
+
